67b956cfbb29d0e7ac79866b1a7b52490f9c0834
inderbir870 <inderbir1234@outlook.com>
2024-11-01 11:57:14 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showString("Down is me")
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . # . .
+        . . . . .
+        . . . # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . # # .
+        . . . # .
+        . . . # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # .
+        . . . # .
+        . . # # .
+        `)
+    basic.showString("YAY")
+})
